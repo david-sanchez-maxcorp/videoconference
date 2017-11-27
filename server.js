faaa01b25297557b3728374de8a961955bccf960
David Camilo Sanchez Mora <david.sanchez96@outlook.com>
2017-11-27 02:07:12 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,105 @@
+/**
+ * Server module.
+ *
+ *
+ */
+
+"use strict";
+var fs = require("fs");
+var https = require("https");
+var nodestatic = require("node-static");
+var express = require("express");
+var path = require("path");
+
+var serverPort = process.env.OPENSHIFT_NODEJS_PORT || 1337;
+var serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || "192.168.1.35";
+var socketIoServer = "192.168.1.35";
+
+////////////////////////////////////////////////
+// SETUP SERVER
+////////////////////////////////////////////////
+var options = {
+  key: fs.readFileSync("./file.pem"),
+  cert: fs.readFileSync("./file.crt")
+};
+
+var app = express();
+require("./router")(app, socketIoServer);
+
+// Static content (css, js, .png, etc) is placed in /public
+app.use(express.static(__dirname + "/public"));
+
+// Location of our views
+app.set("views", __dirname + "/views");
+
+// Use ejs as our rendering engine
+app.set("view engine", "ejs");
+
+// Tell Server that we are actually rendering HTML files through EJS.
+app.engine("html", require("ejs").renderFile);
+
+var server = https.createServer(
+  {
+    key: fs.readFileSync("./file.pem"),
+    cert: fs.readFileSync("./file.crt"),
+    requestCert: false,
+    rejectUnauthorized: false
+  },
+  app
+);
+
+server.listen(1337);
+
+var io = require("socket.io").listen(server);
+
+////////////////////////////////////////////////
+// EVENT HANDLERS
+////////////////////////////////////////////////
+
+io.sockets.on("connection", function(socket) {
+  function log() {
+    var array = [">>> Message from server: "];
+    for (var i = 0; i < arguments.length; i++) {
+      array.push(arguments[i]);
+    }
+    socket.emit("log", array);
+  }
+
+  socket.on("message", function(message) {
+    log("Got message: ", message);
+    socket.broadcast.to(socket.room).emit("message", message);
+  });
+
+  socket.on("create or join", function(message) {
+    var room = message.room;
+    socket.room = room;
+    var participantID = message.from;
+    configNameSpaceChannel(participantID);
+
+    var numClients = io.sockets.clients(room).length;
+
+    log("Room " + room + " has " + numClients + " client(s)");
+    log("Request to create or join room", room);
+
+    if (numClients == 0) {
+      socket.join(room);
+      socket.emit("created", room);
+    } else {
+      io.sockets.in(room).emit("join", room);
+      socket.join(room);
+      socket.emit("joined", room);
+    }
+  });
+
+  // Setup a communication channel (namespace) to communicate with a given participant (participantID)
+  function configNameSpaceChannel(participantID) {
+    var socketNamespace = io.of("/" + participantID);
+
+    socketNamespace.on("connection", function(socket) {
+      socket.on("message", function(message) {
+        // Send message to everyone BUT sender
+        socket.broadcast.emit("message", message);
+      });
+    });
+  }
+});
